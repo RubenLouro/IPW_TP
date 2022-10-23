e3923e6d3fe9c3171b601d12b81c6153b1e19bcc
RubenLouro <80894091+RubenLouro@users.noreply.github.com>
2022-10-23 03:07:11 +0100

    Delete main.mjs

--- main.mjs
+++
@@ -1,9 +0,0 @@
-const INPUT_FILE_NAME = './movie-file.json'
-const OUTPUT_FILE_NAME = './Output.json'
-
-import {readFile, writeFile } from 'node:fs/promises'
-
-import fetch from 'node-fetch'
-
-
-writeFile(OUTPUT_FILE_NAME, "lol")
